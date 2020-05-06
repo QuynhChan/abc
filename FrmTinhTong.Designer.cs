2aab3592df9ea73c38f8253b657f2e292d634212
= <=>
2020-05-06 22:09:02 +0700

    cmt

---
+++ FrmTinhTong.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Chuong4_Part1
+{
+    partial class FrmTinhTong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lable1 = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.toolTip2 = new System.Windows.Forms.ToolTip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LamLai = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lable1
+            // 
+            this.lable1.AccessibleRole = System.Windows.Forms.AccessibleRole.TitleBar;
+            this.lable1.AutoSize = true;
+            this.lable1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lable1.Location = new System.Drawing.Point(102, 63);
+            this.lable1.Name = "lable1";
+            this.lable1.Size = new System.Drawing.Size(118, 29);
+            this.lable1.TabIndex = 0;
+            this.lable1.Text = "Nhap a =";
+            this.lable1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Location = new System.Drawing.Point(226, 63);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(224, 26);
+            this.txtSoA.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(103, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nhap b =";
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Location = new System.Drawing.Point(228, 128);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(222, 26);
+            this.txtSoB.TabIndex = 3;
+            this.txtSoB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoB_KeyPress);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.Tag = "Nhap so nguyen";
+            // 
+            // toolTip2
+            // 
+            this.toolTip2.Tag = "Nhap so nguyen";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(107, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Tong";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(520, 254);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 37);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Thoat";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "label1";
+            // 
+            // LamLai
+            // 
+            this.LamLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LamLai.Location = new System.Drawing.Point(270, 249);
+            this.LamLai.Name = "LamLai";
+            this.LamLai.Size = new System.Drawing.Size(167, 42);
+            this.LamLai.TabIndex = 8;
+            this.LamLai.Text = "Lam lai";
+            this.LamLai.UseVisualStyleBackColor = true;
+            this.LamLai.Click += new System.EventHandler(this.LamLai_Click);
+            // 
+            // FrmTinhTong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LamLai);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.lable1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Name = "FrmTinhTong";
+            this.Text = "Chuong Trinh Tinh Tong";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lable1;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ToolTip toolTip2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button LamLai;
+    }
+}
---
+++ FrmTinhTong.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuong4_Part1
+{
+    public partial class FrmTinhTong : Form
+    {
+        public FrmTinhTong()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSoA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) ||
+(e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8)
+|| (Convert.ToInt32(e.KeyChar) == 13))
+
+                e.Handled = false;
+
+            else e.Handled = true;
+        }
+
+        private void txtSoB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (((e.KeyChar >= '0') && (e.KeyChar <= '9')) ||
+(e.KeyChar == '-') || (e.KeyChar == '.') || (Convert.ToInt32(e.KeyChar) == 8)
+|| (Convert.ToInt32(e.KeyChar) == 13))
+
+                e.Handled = false;
+
+            else e.Handled = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int SoA, SoB;
+            if (txtSoA.Text == "")
+
+{
+                MessageBox.Show(" Ban phai nhap gia tri cho a"," Thong bao ",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtSoA.Focus();
+               
+               return;
+            }
+            if (txtSoB.Text =="")
+{
+                MessageBox.Show("Ban phai nhap gia tri cho b"," Thong bao ",
+                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtSoB.Focus();
+                
+                return;
+            }
+
+                SoA = Convert.ToInt32(txtSoA.Text);
+                SoB = Convert.ToInt32(txtSoB.Text);
+            int button1 = 0;
+            if (SoA < SoB)
+            {
+                for (int i = SoA; i <= SoB; i++)
+                { button1 = button1 + i; }
+
+            }
+            if (SoA > SoB)
+            {
+                for (int i = SoB; i <= SoA; i++)
+                { button1 = button1 + i; }
+            }
+            label1.Text = "Tong cac so tu " + SoA + " den " + SoB + " = " + button1.ToString();
+
+            {
+
+        }
+    }
+
+
+        private void LamLai_Click(object sender, EventArgs e)
+        {
+            txtSoA.Text = "";
+            txtSoB.Text = "";
+            button1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ban ca muon thoat khong","Thong bao",
+              MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==System.Windows.Forms.DialogResult.Yes)
+              Application.Exit();
+        }
+    }
+}
